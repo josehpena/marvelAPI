f62efccd62c44c04747860c42af632ea611951bc
Jose Pena <josehpena2@gmail.com>
2020-02-16 15:30:22 -0300

    initial structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+mongoose.connect('mongodb+srv://usertest:<password>@marvelapi-pena-7yc2a.mongodb.net/test',
+{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);

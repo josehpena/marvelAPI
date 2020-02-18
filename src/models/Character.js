692776138fb0fce87dffb2183278f8c0f978fffe
jose pena <josepena@planeta.inf.br>
2020-02-18 13:33:43 -0300

    Character Schema created

--- src/models/Character.js
+++ src/models/Character.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const SchemaList = require("./utils/SchemaLists");
 
-const charSchema = new mongoose.Schema({
-    id (int, optional)            : 
-    name (string, optional)       : 
-    description (string, optional): 
-    modified (Date, optional)     :  
-    comics (ComicList, optional)  : 
-    stories (StoryList, optional) : 
-    events (EventList, optional)  : 
-    series (SeriesList, optional) : 
-}); 
+const CharSchema = new mongoose.Schema({
+    _id: { type: Schema.ObjectId, auto: true },
+    name: { type: String, required: true },
+    description: { type: String, required: false },
+    modified: { type: Date, required: false },
+    comics: { type: [SchemaList.ComicList], required: false },
+    stories: { type: [SchemaList.StoryList], required: false },
+    events: { type: [SchemaList.EventList], required: false },
+    series: { type: [SchemaList.SeriesList], required: false }
+});
+
+module.exports = mongoose.model("Character", CharSchema);
---
+++ src/models/utils/SchemaLists.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const ComicList = new mongoose.Schema({
+    avaible: { type: Number, required: false },
+    returned: { type: Number, required: false },
+    items: [name]
+});
+
+const StoryList = new mongoose.Schema({
+    avaible: { type: Number, required: false },
+    returned: { type: Number, required: false },
+    items: [StorySummary]
+});
+
+const StorySummary = new mongoose.Schema({
+    name: String,
+    type: String
+});
+
+const EventList = new mongoose.Schema({
+    avaible: { type: Number, required: false },
+    returned: { type: Number, required: false },
+    items: [name]
+});
+
+const SeriesList = new mongoose.Schema({
+    avaible: { type: Number, required: false },
+    returned: { type: Number, required: false },
+    items: [name]
+});
+
+exports.ComicList = ComicList;
+exports.StoryList = StoryList;
+exports.EventList = EventList;
+exports.SeriesList = SeriesList;
